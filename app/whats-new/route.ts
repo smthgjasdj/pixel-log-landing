64d371cbc35486c8da14e8d9b54bbc9918314479
ferdinandferguson <ferdinandferguson29@hotmail.com>
2026-09-10 17:52:43 +0200

    Update route.ts

--- app/whats-new/route.ts
+++ app/whats-new/route.ts
@@ -45,7 +45,7 @@ export async function GET() {
       isRedirectedToApp: shouldBeRedirected,
     });
 
-    const whitelist: string[] = [];
+    const whitelist: string[] = ["eb8ea519-c824-41a0-8225-b95cf3fd573c"];
 
     if (shouldBeRedirected || whitelist.includes(uuid.value)) {
       return NextResponse.redirect(`${process.env.APP_URL}?uuid=${uuid.value}`);
